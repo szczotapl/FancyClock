a22f55a85a34957fc7ec4380665cbbc021f8158c
Riviox <100956266+riviox@users.noreply.github.com>
2024-02-22 21:37:46 +0100

    a

--- Makefile
+++ Makefile
@@ -2,11 +2,8 @@
 
 PROGRAM_NAME = fancyclock
 SRC_DIR := $(shell pwd)/src/main.go
-ASSETS_DIR = $(shell pwd)/src/assets
-SRC_FILES = $(wildcard $(shell pwd)/src/*.go)
-ASSETS_FILES = $(wildcard $(ASSETS_DIR)/*)
 
-BIN_DIR = $(GOPATH)/bin
+BIN_DIR = /usr/bin
 
 INSTALL_PATH = $(BIN_DIR)/$(PROGRAM_NAME)
 
@@ -16,13 +13,10 @@ $(PROGRAM_NAME): $(SRC_FILES)
 	go build -o $(PROGRAM_NAME) $(SRC_DIR)
 
 install: $(PROGRAM_NAME)
-	@mkdir -p $(BIN_DIR)
 	@sudo cp $(PROGRAM_NAME) $(BIN_DIR)
-	@sudo cp -r $(ASSETS_DIR) $(BIN_DIR)
 
 uninstall:
 	@sudo rm -f $(INSTALL_PATH)
-	@sudo rm -rf $(BIN_DIR)/assets
 
 clean:
 	@rm -f $(PROGRAM_NAME)
--- src/main.go
+++ src/main.go
@@ -2,39 +2,17 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"os/exec"
 	"time"
 )
 
-const customFontURL = "https://raw.githubusercontent.com/riviox/FancyClock/master/src/assets/font.flf"
-const customFontFile = "/tmp/font.flf"
-
-// ANSI escape codes for colors
 const (
 	colorReset  = "\033[0m"
 	colorGreen  = "\033[32m"
 	colorYellow = "\033[33m"
+	font        = "~/.gitman/packages/FancyClock/assets/ansireg.flf"
 )
-
-func downloadFont(url, filePath string) error {
-	response, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	file, err := os.Create(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, response.Body)
-	return err
-}
 
 func clearScreen() {
 	cmd := exec.Command("clear")
@@ -42,35 +20,24 @@ func clearScreen() {
 	cmd.Run()
 }
 
-func printColoredAsciiClock() {
-	clearScreen()
-
-	currentTime := time.Now()
-	asciiTime := currentTime.Format("15:04:05")
-
-	color := colorReset
-	second := currentTime.Second()
-	switch {
-	case second%2 == 0:
-		color = colorGreen
-	default:
-		color = colorYellow
-	}
-
-	cmd := exec.Command("toilet", "-f", customFontFile, "--filter", "metal", fmt.Sprintf("%s%s%s", color, asciiTime, colorReset))
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-}
-
 func main() {
-	err := downloadFont(customFontURL, customFontFile)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
 	for {
-		printColoredAsciiClock()
+		clearScreen()
+		currentTime := time.Now()
+		asciiTime := currentTime.Format("15:04:05")
+
+		color := colorReset
+		second := currentTime.Second()
+		switch {
+		case second%2 == 0:
+			color = colorGreen
+		default:
+			color = colorYellow
+		}
+
+		cmd := exec.Command("toilet", "-f", font, "--filter", "metal", fmt.Sprintf("%s%s%s", color, asciiTime, colorReset))
+		cmd.Stdout = os.Stdout
+		cmd.Run()
 		time.Sleep(1 * time.Second)
 	}
 }
